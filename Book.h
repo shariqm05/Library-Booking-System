a13f3867cbc34f13da49fe9a3b0e72a356c2ac4f
shariqm05 <shariqm2005@gmail.com>
2024-10-05 15:59:29 -0400

    Header Files General Structure created the functions, documentation, and general structure for both header files

--- Book.h
+++ Book.h
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+#ifndef BOOK_H //Header Guards
+#define BOOK_H //Header Guards
+
+class Book{
+ public:
+  //Name: Book - Default Constructor
+  //Precondition: None
+  //Postcondition: Creates a default book (0, "default", "default", 0.00)
+  Book();
+  //Name: Book - Overloaded Constructor
+  //Precondition: Requires year, title, author, and their aggregated score (rating)
+  //Postcondition: Creates a book based on passed parameters
+  Book(int year, string title, string author,  double score);
+
+  //Name: Mutators and Accessors
+  //Precondition: None
+  //Postcondition: Sets and Gets private member variables
+  int GetYear(); //return year
+  string GetTitle(); // return title
+  string GetAuthor(); // return author(s)
+  double GetScore(); // return rating score
+  void SetYear(int year); //sets year
+  void SetTitle(string title); //sets title
+  void SetAuthor(string author); //sets author(s)
+  void SetScore(double score); //sets score
+
+  //Name: DisplayBook
+  //Precondition: Requires book variables to be populated
+  //Postcondition: None
+  //Desc: Displays the information about the book
+  void DisplayBook(); //displays information about book with the format:
+  //                  *Title* (*Year*) by *Author(s)* with a score of *Score*
+private:
+  int m_year; //Year of book
+  string m_title; //Title of book
+  string m_author; //Author(s) of book
+  double m_score; //Aggregated score (rating)
+};
+
+#endif
+
 
--- Library.h
+++ Library.h
@@ -1,1 +1,69 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Book.h"
+
+using namespace std;
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+//Constants
+const int NUM_BOOKS = 965; //Total number of books in input file
+const int MAX_WAIT = 5; //Maximum books on user wait list
+const char DELIMETER = '|'; //Symbol between each variable in input file
+const int INCR = 10; //Number of books displayed at a time (10 per screen)
+
+class Library{
+ public:
+  //Name: Library - Default Constructor
+  //Precondition: None
+  //Postcondition: Creates a new Library
+  Library();
+  //Name: Library - Overloaded Constructor
+  //Precondition: None
+  //Postcondition: Creates a new Library and passes the book filename
+  Library(string filename);
+  //Name: LoadCatalog(string filename)
+  //Precondition: Requires filename
+  //Postcondition: Loads the file into the m_bookCatalog (as books)
+  //Note: You are permitted to use stoi (string to integer) and stod
+  // (string to double) to convert during loading of catalog
+  void LoadCatalog(string filename);
+  //Name: MainMenu
+  //Precondition: None
+  //Postcondition: Manages the application and the menu
+void MainMenu();
+  //Name: DisplayBooks
+  //Precondition: Displays the master list of all books in catalog
+  //Postcondition: Lists all books (numbered starting at 1)
+  //Note: Displays 10 books per screen using constant INCR
+  //If the user enters Q or q, the program stops displaying
+  //  books and returns to main menu else displays next 10 books
+  void DisplayBooks();
+  //Name: SearchCatalog()
+  //Precondition: Requires m_bookCatalog
+  //Postcondition: Searches m_bookCatalog by title
+  //Note: Use string function of .find to search for any part of string
+  void SearchCatalog();
+  //Name: AddBook
+  //Precondition: m_bookCatalog is populated
+  //Postcondition: Displays the current list and copies a book to m_waitList
+  //               Increments m_waitCount and limits capacity to MAX_WAIT
+  void AddBook();
+  //Name: BookExist(Book)
+  //Precondition: m_bookCatalog is populated
+  //Postcondition: Helper function to check if passed book exists in m_waitList. Returns true if exists in m_waitList else false
+  bool BookExist(Book book);
+  //Name: DisplayWaitList
+  //Precondition: m_bookCatalog is populated
+  //Postcondition: Displays the WaitList using DisplayBook
+  void DisplayWaitList();
+private:
+  Book m_bookCatalog [NUM_BOOKS]; //Holds all books in file
+  Book m_waitList [MAX_WAIT]; //Holds all books in wait list
+  int m_waitCount; //Keeps track of how many books on wait list
+};
+
+#endif
+
 
