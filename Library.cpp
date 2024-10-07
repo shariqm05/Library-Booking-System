f5fb39d9391e8a447d6d97cac02c5702360ff601
shariqm05 <shariqm2005@gmail.com>
2024-10-06 22:50:19 -0400

    BookExist(), AddBook(), and MainMenu() BookExist():     Completed this function. Used in AddBook as a helper function to see     if the book trying to be added to the waitlist is already on the     waitlist. This function also implements GetTitle from the Book class.
    AddBook():
        Completed this function. Used to add a book from the library catalog
        to the waitlist. Waitlist is restricted to only 5 books per MAX_WAIT
        This function also implements DisplayBooks().
    
    MainMenu():
        Function is moslty complete. outputs a commnad list for different
        functions in the prorgram, followed by a switch statement to handle
        all input cases, including invalid ones. The only thing that is
        needed to complete this function is the logic implementation of the
        others so that it works as inteded.
    
        IMPORTANT: DisplayBooks() and the other functions need to be
        completed before this works as intended. In terms of logic of each

--- Library.cpp
+++ Library.cpp
@@ -2,6 +2,7 @@
 
 //LoadCatalog()
 void Library::LoadCatalog(string filename){
+
     ifstream file(filename);
 
     string line; //represents a a single line, for a single book's info
@@ -15,32 +16,119 @@ void Library::LoadCatalog(string filename){
           string author; //Author(s) of book
           double score; //Aggregated score (rating)
           string strYear, strScore; //convert these to int and double using stoi and stod
-          size_t pos;
+          int pos; //int to track position while reading in the line
 
-          // for finsing an intialzing the year. Coverted to int using stoi
+          // for finding an intialzing the year. Coverted to int using stoi
           pos = line.find(DELIMETER);
           year = stoi(line.substr(0,pos));
           line.erase(0,pos+1); //erase that part of the line
+          
           // for title
           pos = line.find(DELIMETER);
           title = line.substr(0,pos);
           line.erase(0,pos+1); //erase that part of the line
+          
           // for author
           pos = line.find(DELIMETER);
           author = line.substr(0,pos);
           line.erase(0,pos+1); //erase that part of the line
+          
           //for score: used stod to convert to souble
           score = stod(line);
 
-          //now take the extracte varaiables and mak a Book object with it
+          //now take the extracted vars and make a Book object with it
           Book aBook(year, title, author, score);
           //put it int the book catalog array
           m_bookCatalog[i] = aBook;
           i++;
         }
+        cout << "Catalog populated with " << i << " books." << endl;
     }
     else{ //input val
       cout << "File Does not exist" << endl;
     }
     file.close();
+  }
+
+//MainMenu()
+void Library::MainMenu(){
+  bool isRunning = true;
+  do{
+    int command;
+    
+    //main menu 
+    cout << "\nWhat would you like to do?" << endl;
+    cout << "1. Display All Books" << endl;
+    cout << "2. Search Catalog for Book Title" << endl;
+    cout << "3. Add Book to Waitlist" << endl;
+    cout << "4. Display Waitlist" << endl;
+    cout << "5. Quit" << endl;
+    cin >> command;
+
+    //for handling input
+    switch(command){
+      case 1: 
+        DisplayBooks();
+        break;
+      case 2: 
+        SearchCatalog();
+        break;
+      case 3: 
+        AddBook();
+        break;
+      case 4: 
+        DisplayWaitList();
+        break;
+      case 5: 
+        isRunning = false; //ends program
+        break;
+      default: cout << "\n";//input val         
+    }
+  } while (isRunning); 
+}
+
+//AddBook()
+void Library::AddBook(){
+    //if waitlist is already full
+    if (m_waitCount >= MAX_WAIT){
+        cout << "No additional books can be added to the wait list." << endl;
+      }
+    else{
+      int bookNum;
+      bool Looping = true;
+      do{
+        cout << "Which book would you like to add? (-1 for list)" << endl;
+        cin >> bookNum;
+
+        //input val
+        if((bookNum < -1) || (bookNum == 0) || bookNum > NUM_BOOKS){
+          cout << "Invalid number" << endl;
+        }
+
+        //for displaying list
+        else if (bookNum == -1){
+          DisplayBooks();
+        }
+
+        //If its already on the waitlist
+        else if(BookExist(m_bookCatalog[bookNum-1])){
+          cout << m_bookCatalog[bookNum-1].GetTitle() << " is already on the waitlist." << endl;
+        }
+
+        else{
+          m_waitList[m_waitCount] = m_bookCatalog[bookNum-1];
+          m_waitCount++;
+          Looping = false;
+        }
+      } while(Looping);
+    } 
+  }
+
+//BookExist()
+bool Library::BookExist(Book book){
+    for (int i = 0; i < m_waitCount; i++){
+      if (m_waitList[i].GetTitle() == book.GetTitle())
+        return true;
+    }
+    return false;
   }
--- Library.h
+++ Library.h
@@ -32,10 +32,12 @@ class Library{
   //Note: You are permitted to use stoi (string to integer) and stod
   // (string to double) to convert during loading of catalog
   void LoadCatalog(string filename);
+
   //Name: MainMenu
   //Precondition: None
   //Postcondition: Manages the application and the menu
-void MainMenu();
+  void MainMenu();
+
   //Name: DisplayBooks
   //Precondition: Displays the master list of all books in catalog
   //Postcondition: Lists all books (numbered starting at 1)
@@ -43,20 +45,24 @@ void MainMenu();
   //If the user enters Q or q, the program stops displaying
   //  books and returns to main menu else displays next 10 books
   void DisplayBooks();
+
   //Name: SearchCatalog()
   //Precondition: Requires m_bookCatalog
   //Postcondition: Searches m_bookCatalog by title
   //Note: Use string function of .find to search for any part of string
   void SearchCatalog();
+
   //Name: AddBook
   //Precondition: m_bookCatalog is populated
   //Postcondition: Displays the current list and copies a book to m_waitList
   //               Increments m_waitCount and limits capacity to MAX_WAIT
   void AddBook();
+    
   //Name: BookExist(Book)
   //Precondition: m_bookCatalog is populated
   //Postcondition: Helper function to check if passed book exists in m_waitList. Returns true if exists in m_waitList else false
   bool BookExist(Book book);
+
   //Name: DisplayWaitList
   //Precondition: m_bookCatalog is populated
   //Postcondition: Displays the WaitList using DisplayBook
@@ -64,7 +70,7 @@ void MainMenu();
 private:
   Book m_bookCatalog [NUM_BOOKS]; //Holds all books in file
   Book m_waitList [MAX_WAIT]; //Holds all books in wait list
-  int m_waitCount; //Keeps track of how many books on wait list
+  int m_waitCount = 0; //Keeps track of how many books on wait list
 };
 
 #endif
